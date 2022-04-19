4c58cf0d70fb87bc9043c054587dc345cd572e1b
koviubi56 <58398849+koviubi56@users.noreply.github.com>
2022-04-19 15:11:47 +0000

    Added keepalive

--- main.py
+++ main.py
@@ -52,6 +52,27 @@ DEBUG = True
 db = pickledb.load("database.db", True)
 
 
+if environ.get("KEEPALIVE", "0") == "1":
+    from fastapi import FastAPI
+    from fastapi.middleware.cors import CORSMiddleware
+    import uvicorn
+    from threading import Thread
+        
+    app = FastAPI()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials=True,
+    )
+        
+    @app.get("/")
+    def index():
+        return 200
+            
+    Thread(target=lambda: uvicorn.run(app, host="0.0.0.0", port=8000), daemon=True).start()
+
 def reload_db() -> Literal[True]:
     """
     Reload the database.
